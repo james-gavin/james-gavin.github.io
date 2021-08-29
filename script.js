b23cf0af63c9477b8e373c9003d1070630bb0f29
BuildTools <unconfigured@null.spigotmc.org>
2021-08-28 22:12:43 -0500

    Update

--- script.js
+++ script.js
@@ -17,12 +17,12 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("countdown").innerHTML = "refresh";
   }
-}, 1000);
+}, 250);
